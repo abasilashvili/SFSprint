af5f5e3fb45c110c605b913bf6362ea61d4a89e0
Aleksi Basilashvili <118545430+abasilashvili@users.noreply.github.com>
2023-10-04 16:19:11 +0300

    Add files via upload

---
+++ pereval/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from pereval.models import *
+
+admin.site.register(PerevalAdded)
+admin.site.register(Coords)
+admin.site.register(PerevalImages)
+admin.site.register(PerevalUser)
